1ea58966fad45410de0b3d192eafa4630c88105e
arijouni <erigeouni2645@gmail.com>
2023-12-25 13:28:20 +0100

    adding files

---
+++ reactapp/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Logo from "./component/Logo"
+
+import PlayersList from './component/PlayersList';
+
+function App() {
+  return (
+    <div className="App">
+      <Logo />
+      <PlayersList/>
+    </div>
+  );
+}
+
+export default App;
